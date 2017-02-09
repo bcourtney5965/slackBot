4dc6649a7c8d2e04140a4c7f0bb8d29e33c56f76
Brandon Courtney <brandon.h.courtney@gmail.com>
2017-02-08 16:03:16 -0800

    Heroku/slack bot tutorial

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -4,6 +4,6 @@ var morgan = require('morgan');
 module.exports = function mountMiddleware(app) {
   // Mount our middleware including bodyParser and morgan middlewares here.
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded());
+  app.use(bodyParser.urlencoded({ extended: true }));
   // app.use(morgan('combined'));
 };
--- server/server.js
+++ server/server.js
@@ -2,22 +2,23 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var router = require('./routes');
+var hellobot = require('./hellobot');
 
 app.use(express.static(path.join(__dirname, '../client')));
-
-// Test Routes
-app.get('/test',(req, res) => {
-  console.log('inside get reqest');
-  res.status(200).json({"AJAX":"get response from Server"});
-})
-app.post('/test',(req, res) => {
-  console.log('inside post reqest');
-  res.status(200).json({"AJAX":"post response from Server"});
-})
 
 // Mount middleware &
 // Expose routes
 require("./middleware")(app);
 app.use('/api', router);
+
+// Test Routes
+app.get('/test', function (req, res) { res.status(200).send('Hello world!') });
+app.post('/hello', hellobot);
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(400).send(err.message);
+});
 
 module.exports = app;
