a10553d8e282eb6f4b4e3e782d0ad7d3049a8902
Brandon Courtney <brandon.h.courtney@gmail.com>
2017-02-08 17:51:17 -0800

    .

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -11,7 +11,6 @@ exports.postController = (req, res) => {
 };
 
 exports.hellobot = (req, res) => {
-  console.log('inside hellobot Controller');
   var userName = req.body.user_name;
   var botPayload = {
     text : 'Hello, ' + userName + '!'
